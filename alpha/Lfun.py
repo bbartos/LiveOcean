53d792ae0773553d2e14412762ff2c9e85caed33
bbartos <bbartosd@gmail.com>
2017-05-19 10:12:41 -0700

    Incorporating change in alpha/Lfun.py

--- alpha/Lfun.py
+++ alpha/Lfun.py
@@ -2,20 +2,20 @@
 Functions for LiveOcean.
 """
 
-def Lstart(gridname='BLANK', tag='BLANK'):    
+def Lstart(gridname='BLANK', tag='BLANK'):
     """
     This is to set environment variables in the LiveOcean system
     using values in a csv file.  It is similar to Lstart.m in the
     MATLAB code, but it returns a dictionary instead of a structure.
-    
+
     We use input parameters to allow for different gridnames and tags to
     coexist.
-    """   
+    """
     # put top level information from input into a dict
     Ldir = dict()
     Ldir['gridname'] = gridname
     Ldir['tag'] = tag
-    
+
     # Build information on the directory structure.
     import os
     which_home = os.environ.get("HOME") # This works even when called by cron.
@@ -41,17 +41,17 @@ def Lstart(gridname='BLANK', tag='BLANK'):
             print('Trouble filling out environment variables in Ldir')
     else:
         print('Trouble filling out environment variables in Ldir')
-    
+
     # and add a few more things
     Ldir['gtag'] = Ldir['gridname'] + '_' + Ldir['tag']
     Ldir['LO'] = Ldir['parent'] + 'LiveOcean/'
     Ldir['LOo'] = Ldir['parent'] + 'LiveOcean_output/'
     Ldir['data'] = Ldir['parent'] + 'LiveOcean_data/'
-    Ldir['res'] = Ldir['LO'] + 'preamble/make_resources/'
-    Ldir['run'] = Ldir['res'] + Ldir['gridname'] +'/'
-       
+    Ldir['grid'] = Ldir['data'] + 'grids/' + Ldir['gridname'] + '/'
+    Ldir['forecast_days'] = 3
+
     return Ldir
-    
+
 def make_dir(dirname, clean=False):
     # Make a directory if it does not exist.
     # Use clean=True to clobber the existing directory.
@@ -65,18 +65,18 @@ def make_dir(dirname, clean=False):
             os.mkdir(dirname)
         except OSError:
             pass # assume OSError was raised because directory already exists
-        
+
 def dict_to_csv(dict_name, csv_name, write_mode='w'):
     # Write the contents of a dict to a two-column csv file.
     # The write_mode can be wb (overwrite, binary) or ab (append binary).
     # Binary mode is better across platforms.
-    # 2015.11.25 changed to 'w' becasue it was throwing an error in python 3
+    # 2015.11.25 changed to 'w' because it was throwing an error in python 3
     import csv
     with open(csv_name, write_mode) as ff:
         ww = csv.writer(ff)
         for kk in dict_name.keys():
             ww.writerow((kk, dict_name[kk]))
-            
+
 def csv_to_dict(csv_name):
     # Reads two-column csv file into a dict.
     import csv
@@ -100,7 +100,33 @@ def run_worker(Ldir, worker_type='matlab'):
         cmd = Ldir['which_matlab']
         run_cmd = [cmd, "-nojvm", "-nodisplay", "-r", func, "&"]
         proc = subprocess.Popen(run_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = proc.communicate() # "out" is the screen output of the matlab code        
+        out, err = proc.communicate() # "out" is the screen output of the matlab code
+        print(out.decode()) # this ends up as part of the make_forcing_main.py screen output
+    else:
+        print('other worker types not implemented yet')
+        
+def run_worker_post(Ldir, worker_type='matlab'):
+    # run the worker code using subprocess
+    #
+    # Passes an extra piece of information: an "indir" used by the
+    # carbon post-processing code. Eventually I may want to separate
+    # out the forcing things from the post-processing things.
+    #
+    if worker_type == 'matlab':
+        # pass arguments to a matlab program
+        import subprocess
+        
+        func = ("make_forcing_worker(\'" +
+            Ldir['gridname'] + "\',\'" +
+            Ldir['tag'] + "\',\'" +
+            Ldir['date_string'] + "\',\'" +
+            Ldir['run_type'] + "\',\'" +
+            Ldir['indir'] + "\',\'" +
+            Ldir['LOogf_f'] + "\')")
+        cmd = Ldir['which_matlab']
+        run_cmd = [cmd, "-nojvm", "-nodisplay", "-r", func, "&"]
+        proc = subprocess.Popen(run_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = proc.communicate() # "out" is the screen output of the matlab code
         print(out.decode()) # this ends up as part of the make_forcing_main.py screen output
     else:
         print('other worker types not implemented yet')
@@ -109,34 +135,34 @@ def datetime_to_modtime(dt):
     # This is where we define how time will be treated
     # in all the model forcing files.
     # NOTE: still need to define time zone (UTC?)
-    
+
     # INPUT:
     # dt is a single datetime value
-    
+
     # this returns seconds since 1/1/1970
     from datetime import datetime
     t = (dt - datetime(1970,1,1,0,0)).total_seconds()
     return t
-    
+
 def modtime_to_datetime(t):
     # input seconds since 1/1/1970 (single number)
     from datetime import datetime, timedelta
     dt = datetime(1970,1,1,0,0) + timedelta(seconds=t)
     return dt
-    
+
 def modtime_to_mdate_vec(mt_vec):
     from datetime import datetime, timedelta
     import matplotlib.dates as mdates
-    
+
     # input numpy vector of seconds since 1/1/1970
-    
+
     # first make a list of datetimes
     dt_list = []
     for mt in mt_vec:
         dt_list.append(datetime(1970,1,1,0,0) + timedelta(seconds=mt))
-    
+
     md_vec = mdates.date2num(dt_list)
 
     return md_vec
-    
+
 
